55bfa6121ac04dbe6b9ba93358d22466e92d18da
venemap <venemap@rose-hulman.edu>
2020-09-10 13:24:55 -0400

    comments and cleanup added to etch_a_sketch, basic readme and install script added

--- hw01/etch_a_sketch_venema.py
+++ hw01/etch_a_sketch_venema.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3
 
-import pygame, sys, os, time
+#setup and imports
+import pygame, sys, os
 from pygame.locals import *
-
 pygame.init()
 
+#initial constants
 white = [255, 255, 255]
 black = [0, 0, 0]
-
 cont = True
 rightPressed = 0
 leftPressed = 0
 upPressed = 0
 downPressed = 0
 
-while cont:
-    screenSize = input("What size screen would you like? Small, Medium, or Large?")
+
+def clearScreen(): #clear screen and draw guidelines 100px apart
+    screen.fill(white)
+    z = 0
+    for z in range(screenSize):
+        pygame.draw.lines(screen, (211, 211, 211), True, [(z * 100, 0), (z * 100, yBound)])
+        pygame.draw.lines(screen, (211, 211, 211), True, [(0, z * 100), (xBound, z * 100)])
+
+
+
+while cont: #take in user input and parse
+    screenSize = input("What size screen would you like? Small, Medium, or Large?\n")
     screenSize = screenSize.lower()
     if screenSize == 'small':
         screenSize = 4
@@ -26,42 +36,44 @@ while cont:
     elif screenSize == 'large':
         screenSize = 8
         cont = False
-    print('Invalid input, please try again \n')
+    else:
+         print('Invalid input, please try again \n') #invalid input, restart prompt
 
+#draw screen
 xBound = 100*screenSize
 yBound = 100*screenSize
-
 screen = pygame.display.set_mode((xBound, yBound))
 
 
 #Set initial placement of cursor and color of screen
 x = int(xBound/2)
 y = int(yBound/2)
-screen.fill(white)
+clearScreen()
 
-while True:
+while True: #main loop
+    #draw and update main circle
     pygame.draw.circle(screen, black, (x,y), 2)
     pygame.display.update()
-    #print('x is: ', x, '\ny is:', y)
 
-    key = pygame.key.get_pressed()
+    key = pygame.key.get_pressed() #array of states for all KB buttons
+    #check for key being pressed
     if key[pygame.K_RIGHT]: 
         if (x < xBound) & ~rightPressed:
-            x+=50
+            x += 50
         rightPressed = 1
-        #time.sleep(0.5)
     if key[pygame.K_LEFT] & ~leftPressed: 
         if x > 0:
-            x-=50
+            x -= 50
         leftPressed = 1
     if key[pygame.K_UP] & ~upPressed: 
         if y > 0:
-            y-=50
+            y -= 50
         upPressed = 1
     if key[pygame.K_DOWN] & ~downPressed: 
         if y < yBound:
-            y+=50
-        downPressed = 1
+            y += 50
+        downPressed = 1 
+    #check for key being released
     if key[pygame.K_RIGHT] == False:
         rightPressed = 0
     if key[pygame.K_LEFT] == False:
@@ -73,11 +85,12 @@ while True:
    
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: #Alt+f4 and red x suport
             sys.exit()
-        elif event.type == KEYDOWN and event.key == K_ESCAPE:
+        elif event.type == KEYDOWN and event.key == K_ESCAPE: #escape also quits the game
             sys.exit()
-        elif event.type == KEYDOWN and event.key == K_SPACE:
-            #clear the screen
-            screen.fill(white)
+        elif event.type == KEYDOWN and event.key == K_SPACE: #pressing space clears the board
+            clearScreen()
+
+
         
---
+++ hw01/readme.txt
@@ -0,0 +1,26 @@
+Hello, welcome to Peter Venema's hw01 etch a sketh project
+
+Button Scheme
+Directional movement with arrow keys
+    Right
+    Up
+    Down
+    Left
+Clearing Screen
+    Spacebar
+Quitting the game
+    Escape
+    X button in top right of window
+
+Prerequisits
+    Python3
+    Pygame installed (run install.sh if pygame is not already installed)
+
+Known Issues
+    ALSA lib pcm.c:8526:(snd_pcm_recover) underrun occurred -- Known issue caused by pygame, not impactful to running the program
+        Can cause user input to be slightly disjointed from prompt
+
+Tutorial on how to run
+    run ./etch_a_sketch_venemap.py in terminal
+    terminal will prompt you for what size screen you want, please enter one of the following: small, medium, or large
+    Program will open in a new window with your desired screen size. See button scheme section above for controls
